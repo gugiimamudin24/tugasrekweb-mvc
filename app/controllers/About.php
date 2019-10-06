02cb6724b775690b1bd5e2a8d196c502da4d8ab3
gugiimamudin24 <gugiimamudin@mail.unpas.ac.id>
2019-10-06 15:18:12 +0700

    membuat kontroller dan method

---
+++ app/controllers/About.php
@@ -0,0 +1,16 @@
+<?php 
+
+class About {
+	public function index($nama = 'Shandika', $pekerjaan = 'Dosen') {
+		echo "Halo, nama saya adalah $nama, saya adalah seorang $pekerjaan";
+	}
+
+	public function page() {
+		echo 'About/page';
+	}
+}
+
+
+
+
+ ?>
--- app/core/App.php
+++ app/core/App.php
@@ -1,9 +1,45 @@
 <?php 
 
 class App {
+	protected $controller = 'Home';
+	protected $method = 'index';
+	protected $params = [];
+
+
 	public function __construct() {
+
 		$url = $this->parseURL();
-		var_dump($url);
+
+		// Controller
+		 if ( file_exists('../app/controllers/' . $url[0] . '.php') ){
+		 	$this->controller = $url[0];
+		 unset($url[0]);
+		 
+
+		 }
+
+		require_once '../app/controllers/' . $this->controller . '.php';
+		$this->controller = new $this->controller;
+
+
+		//method
+		if ( isset ($url[1]) ) {
+			if (method_exists($this->controller, $url[1]) ) {
+				$this->method = $url[1];
+				unset($url[1]);
+			}
+		}
+
+
+		//params
+		if ( !empty($url) ) {
+
+			$this->params = array_values($url);
+		}
+
+		// jalankan controller dan method, serta kirimkan params jika ada
+		call_user_func_array([$this->controller, $this->method], $this->params);
+
 	}
 
 
@@ -11,10 +47,11 @@ class App {
 		if ( isset($_GET['url']) ) {
 			// menghilangkan garis miring
 			$url = rtrim($_GET['url'], '/');
+			//menghilangkan simbol atau karakter yang ngaco
 			$url = filter_var($url, FILTER_SANITIZE_URL);
 			$url = explode('/', $url);
 			return $url;
-			//melakukan routing dari halaman index
+
 		}
 	}
 }
