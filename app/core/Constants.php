77b1484b4dfcf5ef2ad479999f4522eea77766d4
gugiimamudin24 <gugiimamudin@mail.unpas.ac.id>
2019-10-07 00:10:44 +0700

    mengelola aset dan mempelajari teknik Bootstrap

---
+++ app/core/Constants.php
@@ -0,0 +1,3 @@
+<?php  
+
+define('BASEURL', 'http://localhost/rekweb/phpmvc/public');
--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,4 +1,7 @@
-
-	<h1>About Me</h1>
+<div class="container">
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/profile.jpg" alt="Gugi Imamudin" width=200 class="rounded-circle shadow" >
 	<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?=$data['umur']; ?>, dan saya adalah seorang <?=$data['pekerjaan']; ?></p>
+
+	</div>
 
