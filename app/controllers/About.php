ab31b98f402ef0bbaec0795e3813bae19d5947a0
gugiimamudin24 <gugiimamudin@mail.unpas.ac.id>
2019-10-06 23:21:20 +0700

    membuat view yang bisa diakses oleh controller

--- app/controllers/About.php
+++ app/controllers/About.php
@@ -1,16 +1,27 @@
 <?php 
 
-class About {
-	public function index($nama = 'Shandika', $pekerjaan = 'Dosen') {
-		echo "Halo, nama saya adalah $nama, saya adalah seorang $pekerjaan";
+class About extends Controller {
+	public function index($nama = 'Gugi', $pekerjaan = 'Mahasiswa', $umur = 22) 
+	{
+		$data['nama'] = $nama;
+		$data['pekerjaan'] = $pekerjaan;
+		$data['umur'] = $umur;
+		$data['judul'] = 'About Me';
+		$this->view('templates/header', $data);
+		$this->view('about/index', $data);
+		$this->view('templates/footer');
 	}
 
-	public function page() {
-		echo 'About/page';
+	public function page()
+	{	
+		$data['judul'] = 'Pages';
+		$this->view('templates/header', $data);
+		$this->view('about/page');
+		$this->view('templates/footer');
 	}
+
+
 }
-
-
 
 
  ?>
