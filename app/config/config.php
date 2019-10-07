9630955ca6d21c0f1552865044c5e41f94d8f8e3
gugiimamudin24 <gugiimamudin@mail.unpas.ac.id>
2019-10-07 12:36:17 +0700

    membuat database wrapper

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php 
+
+define('BASEURL', 'http://localhost/rekweb/phpmvc/public');
+
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
+
+
+
+
+ ?>
--- app/models/Mahasiswa_model.php
+++ app/models/Mahasiswa_model.php
@@ -1,58 +1,29 @@
 <?php
 
 class Mahasiswa_model {
-	private $dbh; //databese handler
-	private $stmt;
+	private $table = 'mahasiswa';
+	private $db;
 
 	public function __construct()
 	{
-		//data source name
-		$dsn = 'mysql:host=localhost;dbname=phpmvc';
+		$this->db = new Database;
+	}	
 
-		try {
-			$this->dbh = new PDO($dsn, 'root', '');
-		} catch(PDOException $e) {
-			die($e->getMessage());
-		}
-
-	}
-
-	// private $mhs =
-	// [
-	// 	[
-	// 		"nama" => "Gugi Imamudin",
-	// 		"nrp" =>  "153040064",
-	// 		"email" => "gugiimamudin@gmail.com",
-	// 		"jurusan" => "Teknik Informatika"
-	// 	],
-	// 	[
-	// 		"nama" => "Muhammad Saeful Ulum",
-	// 		"nrp" =>  "153040082",
-	// 		"email" => "aepige@gmail.com",
-	// 		"jurusan" => "Teknik Informatika"
-	// 	],
-	// 	[
-	// 		"nama" => "Dede Firmansyah",
-	// 		"nrp" =>  "153040069",
-	// 		"email" => "putrabambu@gmail.com",
-	// 		"jurusan" => "Teknik Informatika"
-	// 	],
-	// 	[
-	// 		"nama" => "Rayi Oktora",
-	// 		"nrp" =>  "153040066",
-	// 		"email" => "rayioktora@gmail.com",
-	// 		"jurusan" => "Teknik Informatika"
-	// 	]
-
-
-	// ];
 
 	public function getAllMahasiswa()
 	{
-		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
-		$this->stmt->execute();
-		return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+		$this->db->query('SELECT * FROM ' . $this->table);
+		return $this->db->resultSet();
 	}
+
+	public function getMahasiswaById ($id) 
+	{
+		$this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+		$this->db->bind('id', $id);
+		return $this->db->single();
+	}
+
+
 
 }
 
